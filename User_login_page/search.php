76eafa6b6dd90811566c5883f855ce56741b6040
Danujan <sivasundaralingamdanujan@gmail.com>
2023-06-11 22:15:33 +0530

    New page for the search results

--- User_login_page/search.php
+++ User_login_page/search.php
@@ -1,45 +1,61 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Search Results
+        </title>
+    </head>
+    <body>
+        <?php
 
-//$con = new PDO("mysql:host = localhost;dbname=draft1;port=3306;username= root; password = ");
-$host = 'localhost';
-$dbname = 'draft1';
-$port = '3306';
-$username = 'root';
-$password = '';
+        //$con = new PDO("mysql:host = localhost;dbname=draft1;port=3306;username= root; password = ");
+        $host = 'localhost';
+        $dbname = 'draft1';
+        $port = '3306';
+        $username = 'root';
+        $password = '';
 
-try {
-    $dsn = "mysql:host=$host;dbname=$dbname;port=$port";
-    $con = new PDO($dsn, $username, $password);
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-}
-//if(isset($_POST["submit"])){
-function search(){
-    $str = $_POST["search"];
-    $sth = $con->prepare("SELECT * FROM search WHERE Name = '$str'");
-    
-    $sth->setFetchMode(PDO::FETCH_OBJ);
-    $sth->execute();
-    
-    if($row = $sth->fetch()){
-        ?>
-        <br><br><br>
-        <table>
-            <tr>
-                <th>Name</th>
-                <th>Description</th>
-            </tr>
-            <tr>
-                <td><?php echo $row->Name; ?></td>
-                <td><?php echo $row->Description; ?></td>
-            </tr>
-        </table>
-<?php
-    }
-        else{
-            echo "Name does not exist";
+        try {
+            $dsn = "mysql:host=$host;dbname=$dbname;port=$port";
+            $con = new PDO($dsn, $username, $password);
+            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
         }
-}
+        //if(isset($_POST["submit"])){
+        function search(){
+            global $con;
+            $str = $_POST["search"];
+            $sth = $con->prepare("SELECT * FROM search WHERE Name = '$str'");
 
-?>
+            $sth->setFetchMode(PDO::FETCH_OBJ);
+            $sth->execute();
+
+            if($row = $sth->fetch()){
+                ?>
+                <br><br><br>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                    </tr>
+                    <tr>
+                        <td><?php echo $row->Name; ?></td>
+                        <td><?php echo $row->Description; ?></td>
+                    </tr>
+                </table>
+        <?php
+            }
+                else{
+                    echo "Name does not exist";
+                }
+        }
+        
+        if(isset($_POST["search"])){
+            search();
+        }
+
+        ?>
+    </body>
+    
+</html>
