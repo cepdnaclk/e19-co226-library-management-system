579a88e760b63ade1f7e4e6674337c7c2742fde3
Thevambikai <110016117+Thevambikai@users.noreply.github.com>
2023-06-12 13:21:02 +0530

    Change book page to show only title total, available qty

--- User_login_thevi/Books.php
+++ User_login_thevi/Books.php
@@ -9,7 +9,7 @@ mysqli_select_db($conn,$db);
 // Assuming you have established a connection to the database
 
 // Fetch book details from the database
-$query = "SELECT * FROM BOOK";
+$query = "SELECT * FROM BOOK_COUNT";
 $result = mysqli_query($conn, $query);
 
 // Check if any rows are returned
@@ -33,19 +33,18 @@ mysqli_close($conn);
             <h1>Book Details</u></h1>
             <table id="Book">
                 <tr>
-                    <th>ISBN</th>
-                    <th>Book_ID</th>
-                    <th>Publisher_ID</th>
-                    <th>Title</th>
+                    <th>Book Title</th>
+                    <th>Total Quantity</th>
+                    <th>Now Available Quantity</th>
                     
                     
                 </tr>
                 <?php foreach($BookArray as $book): ?>
                 <tr>
-                    <td><?php echo $book['ISBN']; ?></td>
-                    <td><?php echo $book['Book_ID']; ?></td>
-                    <td><?php echo $book['Publisher_ID']; ?> </td>
-                    <td><?php echo $book['Title'] ; ?> </td>
+                    <td><?php echo $book['Title']; ?></td>
+                    <td><?php echo $book['Total_qty']; ?></td>
+                    <td><?php echo $book['Now_available_qty']; ?> </td>
+                    
                     
                     
                 </tr>
