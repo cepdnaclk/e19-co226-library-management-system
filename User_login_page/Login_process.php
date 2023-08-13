aa23a91c4e13985b8ac5f0537826855a09e8d88b
Danujan <sivasundaralingamdanujan@gmail.com>
2023-08-13 09:30:50 +0530

    Redirect part changed

--- User_login_page/Login_process.php
+++ User_login_page/Login_process.php
@@ -18,9 +18,12 @@ if ($_POST) {
     $query = "SELECT * FROM users WHERE NationalID = '$NIC' AND Password = '$Password'";
     $result = mysqli_query($conn, $query);
 
-    if ($result && mysqli_num_rows($result) == 1) {
-        // Login successful, retrieve user type from "librarian" or "member" table
+    // Initialize the error message variable
+    //$error = "";
 
+    // Login successful, retrieve user type from "librarian" or "member" table
+    if ($result && mysqli_num_rows($result) == 1) {
+        /*
         // Check if the user is a librarian
         $query_LIB = "SELECT * FROM librarian WHERE National_ID = '$NIC'";
         $result_LIB = mysqli_query($conn, $query_LIB);
@@ -33,21 +36,58 @@ if ($_POST) {
             // User is a librarian
             session_start();
             $_SESSION['auth'] = 'true';
-            header('location: http://localhost/e19-co226-library-management-system/User_login_thevi/Librarian.php');
+            //header('location: http://localhost/e19-co226-library-management-system/User_login_thevi/Librarian.php');
+            // Redirect the user to the welcome page and pass the name as a parameter in the URL
+            header("Location: http://localhost/e19-co226-library-management-system/User_login_thevi/Librarian.php?name=" . urlencode($name));
+
             exit; // Add exit to stop further execution
         } elseif ($result_MEM && mysqli_num_rows($result_MEM) == 1) {
             // User is a member
             session_start();
             $_SESSION['auth'] = 'true';
-            header('location: http://localhost/e19-co226-library-management-system/User_login_thevi/Member.php');
+            //header('location: http://localhost/e19-co226-library-management-system/User_login_thevi/Member.php');
+            // Redirect the user to the welcome page and pass the name as a parameter in the URL
+            header("Location: Member.php?name=" . urlencode($name) . "&memID=" . urlencode($memID));
             exit; // Add exit to stop further execution
         } else {
             // Invalid User Type
             $error = "Invalid User Type";
+        }*/
+        $query1 = "SELECT LastName FROM Person WHERE NationalID='$NIC' LIMIT 1";
+        $query2 = "SELECT National_ID FROM Member WHERE National_ID='$NIC' LIMIT 1";
+        $query_memid = "SELECT Member_ID FROM Member WHERE National_ID='$NIC' LIMIT 1";
+        $query3="SELECT National_ID FROM Librarian WHERE National_ID='$NIC' LIMIT 1";
+        $result1 = mysqli_query($conn, $query1);
+        $result2 = mysqli_query($conn, $query2);
+        $result_mem = mysqli_query($conn, $query_memid);
+        $result3 = mysqli_query($conn, $query3);
+        
+        if (mysqli_num_rows($result1) == 1 && mysqli_num_rows($result2) == 1) {
+            // Retrieve the Member last name from the database
+            $row = mysqli_fetch_assoc($result1);
+            $name = $row['LastName'];
+            $row1 = mysqli_fetch_assoc($result_mem);
+            $memID = $row1['Member_ID'];
+
+            // Redirect the user to the welcome page and pass the name as a parameter in the URL
+            header("Location: http://localhost/e19-co226-library-management-system/User_login_thevi/Member.php?name=" . urlencode($name) . "&memID=" . urlencode($memID));
+
+            exit();
+        }else if(mysqli_num_rows($result1) == 1 && mysqli_num_rows($result3) == 1){
+            // Retrieve the Librarian last name from the database
+            $row = mysqli_fetch_assoc($result1);
+            $name = $row['LastName'];
+
+            // Redirect the user to the welcome page and pass the name as a parameter in the URL
+            header("Location: http://localhost/e19-co226-library-management-system/User_login_thevi/Librarian.php?name=" . urlencode($name));
+            exit();
         }
     } else {
         // Wrong Username or Password
         $error = "Wrong Username or Password";
+        // Redirect back to Login.php with the error message as a URL parameter
+        header('location: http://localhost/e19-co226-library-management-system/Login.php?error=' . urlencode($error));
+        exit;
     }
 
     mysqli_close($conn);
